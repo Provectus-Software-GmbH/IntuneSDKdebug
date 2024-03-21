838539d354c771b00cb941336fbc2ac76ca131cf
tomzeni <thomas.zeni@provectus.de>
2024-03-21 08:35:41 +0100

    Initial Commit

---
+++ IntuneSDKDebug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IntuneSDKDebug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ IntuneSDKDebug/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Intune.MAM;
+
+namespace IntuneSDKDebug;
+
+public partial class MainPage : ContentPage
+{	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	// Open Intune Diagnostic Console
+	private async void OpenIntuneDiagnosticClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			// this does NOT work on an .net8 Maui Release Binary (!)
+			IntuneMAMDiagnosticConsole.DisplayDiagnosticConsole();
+		}
+		catch (Exception ex)
+		{
+            await DisplayAlert("Error", ex.Message, "OK");
+		}
+    }
+
+    // Get Intune Logs and show it via DisplayAlert
+    private async void ShowIntuneDiagnosticLogs(object sender, EventArgs e)
+    {
+        try
+        {
+            // this works on Release AND Debug Binary as expected
+            var logs = IntuneMAMDiagnosticConsole.DiagnosticInformation;
+
+			await DisplayAlert("Intune Logs", logs.ToString(),"OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
+}
+
+
